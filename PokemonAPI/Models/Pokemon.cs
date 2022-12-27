a214768a495ffd43b8bebf7fc373dfcacfce5ccb
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2022-12-27 19:15:58 -0300

    Init

--- PokemonAPI/Models/Pokemon.cs
+++ PokemonAPI/Models/Pokemon.cs
@@ -1,9 +1,10 @@
 ﻿namespace PokemonAPI.Models
 {
-    public class Class
+    public class Pokemon
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Birth { get; set; }
+
     }
 }
---
+++ PokemonAPI/Models/Reviwer.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonAPI.Models
+{
+    public class Reviwer
+    {
+        public int Id { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+    }
+}
