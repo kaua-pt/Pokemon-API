d5d7ee91e3f72238e7c2bb4522dce3ff0cf07b40
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2022-12-28 15:30:25 -0300

    Add contexts

--- PokemonAPI/Models/Pokemon.cs
+++ PokemonAPI/Models/Pokemon.cs
@@ -4,8 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Birth { get; set; }
+        public DateTime BirthDate { get; set; }
         public ICollection<Review> Reviews { get; set; }
-
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
     }
 }
